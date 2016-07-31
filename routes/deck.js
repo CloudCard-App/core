e1b986cd0b5950e36db75df2acbf63a9ac5899b5
Marc Bacvanski <marc.bacvanski@gmail.com>
2016-07-30 22:31:47 -0700

    Fix deck deleting to not do deep delete

--- routes/deck.js
+++ routes/deck.js
@@ -1,4 +1,4 @@
-module.exports.post_create = function(req, res) {
+module.exports.post_create = function (req, res) {
   let name = req.body.name;
   let classID = req.body.classID;
   let deckModel = require('../model/deck').model;
@@ -14,50 +14,44 @@ module.exports.post_create = function(req, res) {
     } else {
       res.status(400).send({error: 'no such class'});
     }
-  }).then(function(newDeck) {
+  }).then(function (newDeck) {
     res.status(200).send(newDeck);
-  }).catch(function(err) {
+  }).catch(function (err) {
     console.log('Error creating deck: ' + err);
     res.status(500).send();
   });
 };
 
-module.exports.get_info = function(req, res) {
+module.exports.get_info = function (req, res) {
   let deckID = req.query.deckID;
   let deckModel = require('../model/deck').model;
 
-  deckModel.findOne({'_id': deckID}).then(function(deck) {
+  deckModel.findOne({'_id': deckID}).then(function (deck) {
     res.status(200).send(deck);
-  }).catch(function(err) {
+  }).catch(function (err) {
     console.error('Error getting info for deck: ' + err);
     res.status(500).send();
   })
 };
 
-module.exports.get_decks = function(req, res) {
+module.exports.get_decks = function (req, res) {
   let classID = req.query.classID;
   let deckModel = require('../model/deck').model;
 
-  deckModel.find({'class._id': classID}).sort({'name': 1}).then(function(decks) {
+  deckModel.find({'class._id': classID}).sort({'name': 1}).then(function (decks) {
     res.status(200).send(decks);
-  }).catch(function(err) {
+  }).catch(function (err) {
     console.error('Error getting decks: ' + err);
     res.status(500).send();
   })
 };
 
-module.exports.delete_deck = function(req, res) {
+module.exports.delete_deck = function (req, res) {
   let deckID = req.body.deckID;
   let deckModel = require('../model/deck').model;
-  let cardModel = require('../model/card').model;
 
-  let deckDeletion = deckModel.findOne({'_id': deckID}).remove();
-  let cardsDeletion = cardModel.find({'deck._id': deckID}).remove();
-
-  Promise.all([deckDeletion, cardsDeletion]).then(function (results) {
-    let deckDeleted = results[0];
-
-    if (JSON.parse(deckDeleted).n == 1) {
+  let deckDeletion = deckModel.findOne({'_id': deckID}).remove().then(function (deleted) {
+    if (JSON.parse(deleted).n == 1) {
       res.status(200).send();
     } else {
       res.status(400).send({error: 'deck not found'});
